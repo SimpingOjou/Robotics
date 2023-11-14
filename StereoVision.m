c31b91bdde8280e9cccb8d5a16a9fd1f37628d58
Chang Hui Simone Lin <daregox@gmail.com>
2023-11-14 17:36:45 +0100

    Last fix stereovision

--- StereoVision.m
+++ StereoVision.m
@@ -10,115 +10,58 @@ videoPlayer = vision.VideoPlayer;
 % Init Robot
 robot = MyRobot();
 assert(robot.is_robot_connected(),"Robot not connected properly");
-%%%%%%%%%% STEREO VISION %%%%%%%%%%
+%%%%%%%%%% IMAGE ACQUISITION %%%%%%%%%%
 robot.move_j(0,-30,-60,0);
 pause(2);
-Pose_left = robot.joint_pos;
-img_left = getsnapshot(vid);
+%Pose = robot.joint_pos;
+img = getsnapshot(vid);
 pause(2);
 
-%robot.move_j(60,-90,-50,-100);
-pause(2)
-Pose_right = robot.joint_pos;
-img_right = getsnapshot(vid);
-pause(2);
 %%%%%%%%%% RED RECOGNITION %%%%%%%%%%
 x_res = 640;
 y_res = 480;
 frame_middle = [x_res/2,y_res/2];
-% detect_bbox_rectangle = [x_res-2,y_res-2,x_res-2,y_res-2];
-% detect_bbox = reshape(bbox2points(detect_bbox_rectangle)', 1, []);
 
 % Subtraction red component from grayscale image to extract red
-diff_im_left = imsubtract(img_left(:,:,1), rgb2gray(img_left));
-diff_im_right = imsubtract(img_right(:,:,1), rgb2gray(img_right));
+diff_im = imsubtract(img(:,:,1), rgb2gray(img));
 %Use a median filter to filter out noise
-diff_im_left = medfilt2(diff_im_left, [3 3]);
-diff_im_right = medfilt2(diff_im_right, [3 3]);
+diff_im = medfilt2(diff_im, [3 3]);
 % Convert the resulting grayscale image into a binary image.
-diff_im_left = imbinarize(diff_im_left,0.25);
-diff_im_right = imbinarize(diff_im_right,0.25);
+diff_im = imbinarize(diff_im,0.25);
 % Remove all those pixels less than 50px
-diff_im_left = bwareaopen(diff_im_left,50);
-diff_im_right = bwareaopen(diff_im_right,50);
+diff_im = bwareaopen(diff_im,50);
 % Label all the connected components in the image
-bw_left = bwlabel(diff_im_left, 8);
-bw_right = bwlabel(diff_im_right, 8);
+bw = bwlabel(diff_im, 8);
 % Image blob analysis
-stats_left = regionprops(bw_left, 'BoundingBox', 'Centroid');
-stats_right = regionprops(bw_right, 'BoundingBox', 'Centroid');
+stats = regionprops(bw, 'BoundingBox', 'Centroid');
 
 % Bound red objects in rectangular box
-for obj = 1:length(stats_left)
-     bbox_left = stats_left.BoundingBox;
-     bbox_right = stats_right.BoundingBox;
+for obj = 1:length(stats)
+     bbox = stats.BoundingBox;
 
-     if ~isempty(bbox_left) && ~isempty(bbox_right)
+     if ~isempty(bbox)
          % Display a bounding box around the detected red.
-         bboxPoints_left = bbox2points(bbox_left(1, :));
-         bboxPolygon_left = reshape(bboxPoints_left', 1, []);
-         center_left = [bbox_left(1)+bbox_left(3)/2,bbox_left(2)+bbox_left(4)/2];
-         img_left = insertShape(img_left, 'Circle',[frame_middle,5],'LineWidth', 5, 'Color',"red");
-         img_left = insertShape(img_left, 'Line',[frame_middle,center_left],'LineWidth', 5, 'Color',"red");       
-         
-         bboxPoints_right = bbox2points(bbox_right(1, :));
-         bboxPolygon_right = reshape(bboxPoints_right', 1, []);
-         center_right = [bbox_right(1)+bbox_right(3)/2,bbox_right(2)+bbox_right(4)/2];
-         img_right = insertShape(img_right, 'Circle',[frame_middle,5],'LineWidth', 5, 'Color',"red");
-         img_right = insertShape(img_right, 'Line',[frame_middle,center_right],'LineWidth', 5, 'Color',"red");
+         bboxPoints = bbox2points(bbox(1, :));
+         bboxPolygon = reshape(bboxPoints', 1, []);
+         center = [bbox(1)+bbox(3)/2,bbox(2)+bbox(4)/2];
+         img = insertShape(data2, 'Polygon', bboxPolygon, 'LineWidth', 3, 'Color',"blue");
+         img = insertShape(img, 'Circle',[frame_middle,5],'LineWidth', 5, 'Color',"red");
+         img = insertShape(img, 'Line',[frame_middle,center],'LineWidth', 5, 'Color',"red");       
      
-         distance_center_left = [abs(frame_middle(1)-center_left(1)), abs(frame_middle(2)-center_left(2))]
-         distance_center_right = [abs(frame_middle(1)-center_right(1)), abs(frame_middle(2)-center_right(2))]
+         distance_center = [abs(frame_middle(1)-center(1)), abs(frame_middle(2)-center(2))]
      end
 
      % Display the annotated video frame
-     %step(videoPlayer, img_left);
-     %step(videoPlayer, img_right);
-     % Check whether the video player window has been closed.
-     if ~isOpen(videoPlayer)
-         break
-     end
+     % step(videoPlayer, img);
 end
 
 % Display stereo images
 figure;
-subplot(1, 2, 1);
-imshow(img_left);
-title('Left Image');
-
-subplot(1, 2, 2);
-imshow(img_right);
-title('Right Image');
-
-% Match features between the two images
-pointsLeft = detectSURFFeatures(rgb2gray(img_left));
-pointsRight = detectSURFFeatures(rgb2gray(img_right));
-
-[featuresLeft, pointsLeft] = extractFeatures(rgb2gray(img_left), pointsLeft);
-[featuresRight, pointsRight] = extractFeatures(rgb2gray(img_right), pointsRight);
-
-indexPairs = matchFeatures(featuresLeft, featuresRight);
-
-matchedPointsLeft = pointsLeft(indexPairs(:, 1));
-matchedPointsRight = pointsRight(indexPairs(:, 2));
-
-% Compute the disparity
-disparityMap = disparity(rgb2gray(img_left), rgb2gray(img_right));
-
-% Convert disparity to depth
-%     Distance: Z = f*B/d 
-%     Where f is focal length, B is baseline (c1 c2 distance)
-%     d is disparity (horizontal offset of the pointed object)
-baseline = abs(Pose_left(1,4)-Pose_right(1,4)); 
-focalLength = 1430; 
-depthMap = (focalLength * baseline) ./ disparityMap;
-
-% Display depth map
-figure;
-imshow(depthMap, []);
+imshow(img);
+title('Image');
 
 %%%%%%%%%% CLEAN UP %%%%%%%%%%
-while (1)
+while true
     if ~isOpen(videoPlayer)
         stop(vid);
         flushdata(vid);
