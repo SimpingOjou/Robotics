2781c08c788cecd726634362eccc420ef8f9408e
fguime6 <150440388+fguime6@users.noreply.github.com>
2023-11-20 15:59:38 +0100

    Update stereovision.m

--- stereovision.m
+++ stereovision.m
@@ -1,6 +1,6 @@
 %%%%%%%%%% INITIALIZATION %%%%%%%%%%
 % Init webcam
-vid = videoinput('winvideo', '2', 'MJPG_640x480');
+vid = videoinput('winvideo', '2', 'MJPG_1280x720');
 set(vid, 'FramesPerTrigger', Inf);
 set(vid, 'ReturnedColorspace', 'rgb');
 vid.FrameGrabInterval = 2;
@@ -12,7 +12,7 @@ robot = MyRobot();
 assert(robot.is_robot_connected(),"Robot not connected properly");
 %%%%%%%%%% IMAGE ACQUISITION %%%%%%%%%%
 robot.move_j(0,0,-90,0);
-pause(2);
+pause(5);
 %Pose = robot.joint_pos;
 img = getsnapshot(vid);
 pause(2);
@@ -24,32 +24,32 @@ pause(2);
 img2 = getsnapshot(vid);
 pause(2);
 
-%image 3
-
-robot.move_j(-90,0,-90,0);
-pause(2);
-%Pose = robot.joint_pos;
-img3 = getsnapshot(vid);
-pause(2);
-
-%Image4
-robot.move_j(-135,0,-90,0);
-pause(2);
-%Pose = robot.joint_pos;
-img4 = getsnapshot(vid);
-pause(2);
-
-%image 5
-robot.move_j(-180,0,-90,0);
-pause(2);
-%Pose = robot.joint_pos;
-img5 = getsnapshot(vid);
-pause(2);
+% % image 3
+% 
+% robot.move_j(-90,0,-90,0);
+% pause(2);
+% Pose = robot.joint_pos;
+% img3 = getsnapshot(vid);
+% pause(2);
+% 
+% %Image4
+% robot.move_j(-135,0,-90,0);
+% pause(2);
+% Pose = robot.joint_pos;
+% img4 = getsnapshot(vid);
+% pause(2);
+% 
+% %image 5
+% robot.move_j(-180,0,-90,0);
+% pause(2);
+% Pose = robot.joint_pos;
+% img5 = getsnapshot(vid);
+% pause(2);
 
 
 %%%%%%%%%% RED RECOGNITION %%%%%%%%%%
-x_res = 640;
-y_res = 480;
+x_res = 1280;
+y_res = 720;
 frame_middle = [x_res/2,y_res/2];
 
 % Subtraction red component from grayscale image to extract red
@@ -57,7 +57,7 @@ diff_im = imsubtract(img(:,:,1), rgb2gray(img));
 %Use a median filter to filter out noise
 diff_im = medfilt2(diff_im, [3 3]);
 % Convert the resulting grayscale image into a binary image.
-diff_im = imbinarize(diff_im,0.25);
+diff_im = imbinarize(diff_im,0.15);
 % Remove all those pixels less than 50px
 diff_im = bwareaopen(diff_im,50);
 % Label all the connected components in the image
@@ -67,7 +67,7 @@ stats = regionprops(bw, 'BoundingBox', 'Centroid');
 
 % Bound red objects in rectangular box
 for obj = 1:length(stats)
-     bbox = stats.BoundingBox;
+     bbox = stats(obj).BoundingBox;
 
      if ~isempty(bbox)
          % Display a bounding box around the detected red.
@@ -108,7 +108,7 @@ for obj = 1:length(stats)
          img2 = insertShape(img2, 'Polygon', bboxPolygon, 'LineWidth', 3, 'Color',"blue");
          img2 = insertShape(img2, 'Circle',[frame_middle,5],'LineWidth', 5, 'Color',"red");
          img2 = insertShape(img2, 'Line',[frame_middle,center],'LineWidth', 5, 'Color',"red");       
-     
+
          distance_center = [abs(frame_middle(1)-center(1)), abs(frame_middle(2)-center(2))]
      end
 
@@ -128,26 +128,25 @@ figure;
 imshow(img2);
 title('Image2');
 
-%im3
-figure;
-imshow(img3);
-title('Image3');
-
-%im4
-figure;
-
-imshow(img4);
-title('Image4');
-
-%im5
-figure;
-imshow(img5);
-title('Image5');
+% %im3
+% figure;
+% imshow(img3);
+% title('Image3');
+% 
+% %im4
+% figure;
+% 
+% imshow(img4);
+% title('Image4');
+% 
+% %im5
+% figure;
+% imshow(img5);
+% title('Image5');
 
 
 %%%%%%%%%% CLEAN UP %%%%%%%%%%
-while true
-    if ~isOpen(videoPlayer)
+        pause (3);
         stop(vid);
         flushdata(vid);
         clear vid;
@@ -156,5 +155,3 @@ while true
         robot.move_j(0,-90,0,0);
         robot.disable_motors();
         clear all;
-    end
-end
